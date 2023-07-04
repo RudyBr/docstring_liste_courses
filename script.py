b7ab81a71acab31f9c9ad45ff881e019b8e407ff
RudyBr <rudybroutin.pro@gmail.com>
2023-07-04 13:00:19 +0200

    creation methodes afficher + sauvegarde

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+import json
+
+import constants
+
 class Liste(list):
     def __init__(self, name):
         self.name = name
@@ -11,8 +16,30 @@ class Liste(list):
         self.append(element)
         return True
        
-
+    def enlever(self, element):
+        if element in self:
+            self.remove(element)
+            return True
+        return False
+    
+    def afficher_liste (self):
+        print(f"Ma liste de {self.name} : ")
+        for element in self:
+            print(f"- {element}")
+        
+    def sauvegarde(self):
+        chemin = Path(constants.DATA_DIR) / f"{self.name}.json"
+        if not Path.exists(constants.DATA_DIR):
+            Path.mkdir(constants.DATA_DIR)
+        with open(chemin, "w") as f:
+            json.dump(self, f, indent=4)
+        
+        return True
 
 if __name__ == "__main__":
-    print("test")
+    liste = Liste("fruits")
+    liste.ajouter("bananes")
+    liste.ajouter("fraises")
+    liste.afficher_liste()
+    liste.sauvegarde()
         
